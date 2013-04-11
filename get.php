46b0538f9d6a4c745f8e6d255bcaacc39f02af68
sethcoder <defectiveseth@gmail.com>
2013-04-11 11:06:35 -0400

    First commit:

---
+++ get.php
@@ -0,0 +1,9 @@
+<?
+
+for($x=1000;$x<3000;$x++) { 
+	system("wget -Oicons_$x.zip http://www.freeiconsdownload.com/Downloads_Files.asp?images_id=$x\&download_id=1");
+}
+
+?>
+
+
---
+++ webmin.sh
@@ -0,0 +1,1 @@
+sudo service webmin restart
