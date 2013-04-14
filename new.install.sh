0caa5215f73ca4ce36bab0dab53a22ffe7230aa3
parson <parson@nova.(none)>
2013-04-13 23:30:46 -0400

    add new.install.sh

---
+++ new.install.sh
@@ -0,0 +1,17 @@
+sudo apt-get install guake -y
+sudo apt-get install mc -y
+sudo apt-get install git-y
+sudo apt-get install sshfs -y
+sudo apt-get install zsh -y
+chsh -s /bin/zsh
+cd ~/Desktop
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+mkdir ~/shell
+cd ~/shell
+git init
+git pull git@github.com:sethcoder/shell.git
+mkdir ~/sites
+mkdir ~/sites/area56
+mkdir ~/sites/defectivemind.com
+mkdir ~/sites/3dnetlabs.info
+mkdir ~/sites/sarahparsonphotography.com
