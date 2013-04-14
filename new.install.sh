cb0539853791582468b36a4e53f35f25faab3daa
parson <parson@nova.(none)>
2013-04-13 23:50:33 -0400

    update

--- new.install.sh
+++ new.install.sh
@@ -1,11 +1,15 @@
+sudo echo 'parson ALL=(ALL) NOPASSWD: ALL\n' >> /etc/sudoers
 sudo apt-get install guake -y
 sudo apt-get install mc -y
 sudo apt-get install git-y
 sudo apt-get install sshfs -y
 sudo apt-get install zsh -y
 chsh -s /bin/zsh
-cd ~/Desktop
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/shell/.zshrc ~/.zshrc
+cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 mkdir ~/shell
 cd ~/shell
 git init
