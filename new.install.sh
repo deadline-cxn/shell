cf8717cba959ff93b83d3e6df4a9ca4b236512f6
sethcoder <defectiveseth@gmail.com>
2013-08-07 20:54:22 -0400

    added codeblocks repository to new install

--- new.install.sh
+++ new.install.sh
@@ -4,6 +4,9 @@ sudo apt-get install mc -y
 sudo apt-get install git-y
 sudo apt-get install sshfs -y
 sudo apt-get install zsh -y
+sudo add-apt-repository ppa:pasgui/ppa ; # codeblocks
+sudo apt-get update
+sudo apt-get install codeblocks -y
 chsh -s /bin/zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/shell/.zshrc ~/.zshrc
