c180728c31c0d99566cfb6760ff27e57c2f6c812
Seth Parson <defectiveseth@gmail.com>
2014-03-13 10:09:25 -0400

    update

---
+++ hi.py
@@ -0,0 +1,83 @@
+import glob
+
+def greet(name):
+	print 'Hello %s' % name
+name = raw_input('What is your name?\n')
+greet(name);
+pa, b = (1, 1)
+while b<100:
+	print '{0} b'.format(b)
+	pa,b = (b,pa+b)
+
+python_files = glob.glob('*.py')
+for file_name in sorted(python_files):
+    print '    ------' + file_name
+
+    with open(file_name) as f:
+        for line in f:
+            print '    ' + line.rstrip()
+
+    print
+
+
+from time import localtime
+
+activities = {8: 'Sleeping',
+              9: 'Commuting',
+              17: 'Working',
+              18: 'Commuting',
+              20: 'Eating',
+              22: 'Resting' }
+
+time_now = localtime()
+hour = time_now.tm_hour
+
+for activity_time in sorted(activities.keys()):
+    if hour < activity_time:
+        print activities[activity_time]
+        break
+else:
+    print 'Unknown, AFK or sleeping!'
+
+
+
+REFRAIN = '''
+%d bottles of beer on the wall,
+%d bottles of beer,
+take one down, pass it around,
+%d bottles of beer on the wall!
+'''
+bottles_of_beer = 99
+while bottles_of_beer > 1:
+    print REFRAIN % (bottles_of_beer, bottles_of_beer, bottles_of_beer - 1)
+    bottles_of_beer -= 1
+
+class BankAccount(object):
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+    def deposit(self, amount):
+        self.balance += amount
+    def withdraw(self, amount):
+        self.balance -= amount
+    def overdrawn(self):
+        return self.balance < 0
+my_account = BankAccount(15)
+my_account.withdraw(5)
+print my_account.balance
+
+
+
+import unittest
+def median(pool):
+    copy = sorted(pool)
+    size = len(copy)
+    if size % 2 == 1:
+        return copy[(size - 1) / 2]
+    else:
+        return (copy[size/2 - 1] + copy[size/2]) / 2
+class TestMedian(unittest.TestCase):
+    def testMedian(self):
+        self.failUnlessEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
+if __name__ == '__main__':
+    unittest.main()
+
