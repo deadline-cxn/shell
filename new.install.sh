98793746a9dd1266c919d026b61eff41b74eb28e
sethcoder <defectiveseth@gmail.com>
2014-04-16 11:18:55 -0400

    stuff

--- new.install.sh
+++ new.install.sh
@@ -1,4 +1,4 @@
-sudo echo 'parson ALL=(ALL) NOPASSWD: ALL\n' >> /etc/sudoers
+sudo echo 'parson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 sudo apt-get install guake -y
 sudo apt-get install mc -y
 sudo apt-get install git-y
@@ -13,12 +13,8 @@ cp ~/shell/.zshrc ~/.zshrc
 cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-mkdir ~/shell
-cd ~/shell
-git init
-git pull git@github.com:sethcoder/shell.git
 mkdir ~/sites
 mkdir ~/sites/area56
 mkdir ~/sites/defectivemind.com
 mkdir ~/sites/3dnetlabs.info
-mkdir ~/sites/sarahparsonphotography.com
+
