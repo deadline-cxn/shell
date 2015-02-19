a53f5856400c7c8c65d7a1f811dd3dc9f85a0b46
sparson <sparson@vm-debian.vm-debian>
2015-02-18 20:20:44 -0500

    Update

--- new.install.sh
+++ new.install.sh
@@ -1,16 +1,19 @@
 sudo echo 'parson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+sudo echo 'sparson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 sudo apt-get install guake -y
 sudo apt-get install mc -y
 sudo apt-get install git-y
 sudo apt-get install sshfs -y
 sudo apt-get install zsh -y
-sudo add-apt-repository ppa:pasgui/ppa ; # codeblocks
-sudo apt-get update
-sudo apt-get install codeblocks -y
+#sudo add-apt-repository ppa:pasgui/ppa ; # codeblocks
+#sudo apt-get update
+#sudo apt-get install codeblocks -y
+echo "Enter password to change shell to zsh "
 chsh -s /bin/zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/shell/.zshrc ~/.zshrc
 cd ~/Downloads
+sudo apt-get install libcurl3 -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 mkdir ~/sites
