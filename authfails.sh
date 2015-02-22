8b3d6bdd7ee4045b3d73fe5fc8b145bdf9b3ea5c
Seth Parson <seth@parson.email>
2015-02-21 22:44:15 -0500

    updates

--- authfails.sh
+++ authfails.sh
@@ -1,4 +1,2 @@
-grep -i 'failed pass' /var/log/*.log |sort -t : -k 1
-
-
+grep -i 'failed pass' /var/log/*.log | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort | uniq | sed 's/^/blockip /' | xargs -i -0 echo '{}'
 
