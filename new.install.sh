dbc68f39b3984c32389cf3b30540283b403270a8
Seth Parson <seth@parson.email>
2015-02-24 11:48:15 -0500

    new.install.script

--- new.install.sh
+++ new.install.sh
@@ -1,26 +1,34 @@
-sudo echo 'parson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
-sudo echo 'sparson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
-sudo apt-get install guake -y
-sudo apt-get install mc -y
-sudo apt-get install git-y
-sudo apt-get install sshfs -y
+distro=$(cat /etc/*-release | grep DISTRIB_ID | sed s/DISTRIB_ID=// | tr '[:upper:]' '[:lower:]')
+codebase=$(cat /etc/*-release | grep DISTRIB_CODENAME | sed s/DISTRIB_CODENAME=//)
+
+echo "New install script for $distro $codebase"
+echo "======================================================================="
+
+#sudo echo 'parson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+#sudo echo 'sparson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+sudo apt-get update
+sudo apt-get install guake terminator mc git sshfs -y
+sudo apt-get install python2.7 python3.4 php5 apache2 mysql-server -y
+
+# ZSH
 sudo apt-get install zsh -y
-#sudo add-apt-repository ppa:pasgui/ppa ; # codeblocks
-#sudo apt-get update
-#sudo apt-get install codeblocks -y
 echo "Enter password to change shell to zsh "
 chsh -s /bin/zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/shell/.zshrc ~/.zshrc
+
+# GOOGLE CHROME
 cd ~/Downloads
-sudo apt-get install libcurl3 -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-mkdir ~/sites
-mkdir ~/sites/area56
-mkdir ~/sites/defectivemind.com
-mkdir ~/sites/3dnetlabs.info
+
+# CODELITE
+sudo apt-key adv --fetch-keys http://repos.codelite.org/CodeLite.asc
+sudo apt-add-repository "deb http://repos.codelite.org/$distro/ $codebase universe"
+sudo apt-get update
+sudo apt-get install codelite wxcrafter -y
+
 git config --global user.name "Seth Parson"
-git config --global user.email defectiveseth@gmail.com
+git config --global user.email "seth@parson.email"
 
-
+echo "======================================================================="
