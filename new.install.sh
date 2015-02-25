046d2f3f83975a927443e8cedb0a9d2f98e6e5c1
Seth Parson <seth@parson.email>
2015-02-24 21:46:16 -0500

    new.install.sh update

--- new.install.sh
+++ new.install.sh
@@ -6,9 +6,16 @@ echo "======================================================================="
 
 #sudo echo 'parson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 #sudo echo 'sparson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
 sudo apt-get update
-sudo apt-get install guake terminator mc sshfs -y
-sudo apt-get install python2.7 python3.4 php5 apache2 mysql-server -y
+
+# MISC NEW INSTALL PROGRAMS
+sudo apt-get install guake terminator mc sshfs gparted -y
+sudo apt-get install gimp youtube-dl -y
+
+# LAMP STUFF
+sudo apt-get install apache2 mysql-server php5 phpmyadmin -y
+
 
 # ZSH
 sudo apt-get install zsh -y
@@ -22,17 +29,20 @@ cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
+########################### DEVELOPMENT SECTION
+
 # CODELITE
 sudo apt-key adv --fetch-keys http://repos.codelite.org/CodeLite.asc
 sudo apt-add-repository "deb http://repos.codelite.org/$distro/ $codebase universe"
 sudo apt-get update
 sudo apt-get install codelite wxcrafter -y
 
-# GIT
+# GIT / SUBVERSION
 sudo apt-get install git -y
 git config --global push.default simple
 git config --global user.name "Seth Parson"
 git config --global user.email "seth@parson.email"
+sudo apt-get install subversion -y
 
 # JAVA
 sudo echo "deb http://ppa.launchpad.net/webupd8team/java/$distro $codebase main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
@@ -42,5 +52,7 @@ sudo apt-get update
 sudo apt-get install oracle-java7-installer -y
 sudo apt-get install oracle-java7-set-default -y
 
+# OTHER DEV STUFF
+sudo apt-get install python2.7 python3.4 ansible lua -y
 
 echo "======================================================================="
