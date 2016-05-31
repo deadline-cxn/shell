a8e33ea668c0e0968a20834f058fdd3fc3101ce1
Seth Parson <seth@parson.email>
2016-05-31 09:54:13 -0400

    update getrepos.php

--- getrepos.php
+++ getrepos.php
@@ -1,6 +1,7 @@
 <?php
 mkdir("/home/sparson/develop");
-chdir("/home/sparson/develop");
+mkdir("/home/sparson/develop/projects");
+chdir("/home/sparson/develop/projects");
 $repos=file_get_contents("/home/sparson/shell/repos");
 $repo_list=explode("\n",$repos);
 foreach($repo_list as $repo) {
