a3b08241ab94181a8383f49b1ec572120d38a74a
sethcoder <seth@parson.email>
2016-08-15 15:02:01 -0400

    cleanup

--- get.php
+++
@@ -1,9 +0,0 @@
-<?
-
-for($x=1000;$x<3000;$x++) { 
-	system("wget -Oicons_$x.zip http://www.freeiconsdownload.com/Downloads_Files.asp?images_id=$x\&download_id=1");
-}
-
-?>
-
-
--- mkv2mp4.sh
+++
@@ -1,4 +0,0 @@
-mkdir mkv
-for i in $(ls *mkv); do ffmpeg -i "$i" -vcodec copy -acodec copy "$i.mp4"; mv "$i" mkv; done;
-
-
