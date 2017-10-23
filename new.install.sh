49e4f8441ae55376926b1b6ca3f68278afff4953
Seth Parson <seth@parson.email>
2017-10-23 12:47:57 -0400

    updates

--- new.install.sh
+++ new.install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 distro=$(cat /etc/*-release | grep DISTRIB_ID | sed s/DISTRIB_ID=// | tr '[:upper:]' '[:lower:]')
 codebase=$(cat /etc/*-release | grep DISTRIB_CODENAME | sed s/DISTRIB_CODENAME=//)
 
@@ -14,30 +16,75 @@ if [ -z "$distro" ]; then
    fi
 fi
 
-echo "New install script for $distro $codebase"
+case "$distro" in
+  *centos*)
+     echo " CentOS detected... Adding EPEL repositories. "
+     distro='centos'
+     sudo yum install epel-release
+     sudo yum update
+     ;;
+  *)
+     sudo apt-get update
+     ;;
+esac
+
+
+echo "New install script for [$distro][$codebase]"
 echo "======================================================================================="
 
-#sudo echo 'parson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
-#sudo echo 'sparson ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+function install {
 
-# sudo apt-get update
+  echo $distro $1
+
+  case "$distro" in
+    *centos*)
+      sudo yum install -y $1
+      ;;
+    *)
+      sudo apt-get install -y $1
+      ;;
+  esac
+}
+
 
 # MISC NEW INSTALL PROGRAMS
-sudo apt-get install guake terminator mc sshfs gparted -y
-sudo apt-get install gimp youtube-dl -y
+install guake
+install terminator
+install mc
+install sshfs
+install gparted
+install gimp
+install youtube-dl
 
 # LAMP STUFF
-sudo apt-get install apache2 mysql-server php5 -y
-sudo apt-get install phpmyadmin -y
-sudo apt-get install apache2-mpm-prefork apache2-utils -y
-sudo apt-get install libapache2-mod-php5 libapr1 libaprutil1 -y
-sudo apt-get install libdbd-mysql-perl libdbi-perl libnet-daemon-perl -y
-sudo apt-get install libplrpc-perl libpq5 mysql-client-5.5 mysql-common -y
-sudo apt-get install php5-gd php5-mysql php5-imagick p7zip-full -y
-sudo apt-get install wkhtmltopdf pev unadf xdms unace -y
+install apache2
+install mysql-server
+install php5
+install phpmyadmin
+install apache2-mpm-prefork
+install apache2-utils
+install libapache2-mod-php5
+install libapr1
+install libaprutil1
+install libdbd-mysql-perl
+install libdbi-perl
+install libnet-daemon-perl
+install libplrpc-perl
+install libpq5
+install mysql-client-5.5
+install mysql-common
+install php5-gd
+install php5-mysql
+install php5-imagick
+install p7zip-full
+install wkhtmltopdf
+install pev
+install unadf
+install xdms
+install unace
 
 # ZSH
-sudo apt-get install zsh -y
+install zsh
 echo "Enter password to change shell to zsh "
 chsh -s /bin/zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
@@ -64,7 +111,8 @@ else
     sudo apt-key adv --fetch-keys http://repos.codelite.org/CodeLite.asc
     sudo apt-add-repository "deb http://repos.codelite.org/$distro/ $codebase universe"
     sudo apt-get update
-    sudo apt-get install codelite wxcrafter -y
+    install codelite
+    install wxcrafter
 fi
 
 # GIT / SUBVERSION
@@ -72,7 +120,7 @@ if [ -f "/usr/bin/git" ]; then
     echo "Git already installed, skipping"
     echo "======================================================================================="
 else
-    sudo apt-get install git -y
+    install git
 fi
 git config --global push.default matching
 git config --global user.name "Seth Parson"
@@ -82,7 +130,7 @@ if [ -f "/usr/bin/svn" ]; then
     echo "Subversion already installed, skipping"
     echo "======================================================================================="
 else
-    sudo apt-get install subversion -y
+    install subversion
 fi
 
 # JAVA
@@ -94,27 +142,36 @@ else
     sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/$distro $codebase main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
     sudo apt-key adv --keyserver "keyserver.$distro.com" --recv-keys EEA14886
     sudo apt-get update
-    sudo apt-get install oracle-java7-installer -y
-    sudo apt-get install oracle-java7-set-default -y
+    install openjdk-8-jre-headless
+    #sudo apt-get install oracle-java8-set-default -y
 fi
 
 # OTHER DEV STUFF
-sudo apt-get install python2.7 -y
-sudo apt-get install python3.2 -y
-sudo apt-get install python-setuptools -y
-sudo apt-get install python-dev -y
-sudo apt-get install lua5.2 -y
+install python2.7
+install python3.2
+install python-setuptools
+install python-dev
+install lua5.2
 
 # OTHER STUFF
-sudo apt-get install toilet nmap docky htop screenfetch openssh-server -y
+install toilet
+install nmap
+install docky
+install htop
+install screenfetch
+install openssh-server
 
 # ANSIBLE
-git clone https://github.com/ansible/ansible.git ~/ansible --recursive
-cd ~/ansible
-chmod +x ~/ansible/setup.py
-sudo ~/ansible/setup.py build
-sudo ~/ansible/setup.py install
+if [ -f "~/ansible/setup.py.build" ]; then
+  git clone https://github.com/ansible/ansible.git ~/ansible --recursive
+  cd ~/ansible
+  chmod +x ~/ansible/setup.py
+  sudo ~/ansible/setup.py build
+  sudo ~/ansible/setup.py install
+fi
 
 echo "======================================================================="
 sudo apt-get upgrade
 sudo apt-get -f install -y
+
+echo 'sparson ALL=(ALL) NOPASSWD: ALL'
