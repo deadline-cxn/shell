c99546b515737e5f400744495a98cebedaa89d6b
Seth Parson <seth@parson.email>
2017-11-02 14:56:45 -0400

    updates

--- getrepos.php
+++ getrepos.php
@@ -1,15 +1,13 @@
 #!/bin/php
 <?php
-//mkdir("/home/sparson/develop");
-//mkdir("/home/sparson/develop/projects");
-// chdir("/home/sparson/develop/projects");
-//echo $argv[1];
+if(empty($argv[1]))
+    $argv[1]="/home/sparson/shell/repos";
 $repos=file_get_contents($argv[1]);
 $repo_list=explode("\n",$repos);
 foreach($repo_list as $repo) {
-	if(!empty($repo)) {
-		$cmd="git clone git@github.com:sethcoder/$repo";
-		echo "$repo ($cmd) \n";
-		//exec($cmd);
-	}
+    if(!empty($repo)) {
+        $cmd="git clone git@github.com:sethcoder/$repo.git";
+        echo "$repo ($cmd) \n";
+        exec($cmd);
+    }
 }
