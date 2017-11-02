04015f3fa9c54c0b284c00168f6535cec3031383
Seth Parson <seth@parson.email>
2017-11-02 14:48:38 -0400

    updates

--- getrepos.php
+++ getrepos.php
@@ -3,7 +3,7 @@
 //mkdir("/home/sparson/develop");
 //mkdir("/home/sparson/develop/projects");
 // chdir("/home/sparson/develop/projects");
-$repos=file_get_contents("repos");
+$repos=file_get_contents("./repos");
 $repo_list=explode("\n",$repos);
 foreach($repo_list as $repo) {
 	if(!empty($repo)) {
