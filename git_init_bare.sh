5055d9b7123d039531b5f98302e51648fedc9c1c
Seth Parson <seth.parson.71@gmail.com>
2018-10-21 00:51:41 -0400

    update git_init_bare.sh

--- git_init_bare.sh
+++ git_init_bare.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 repo_folder="/gitrepos";
-su git
-mkdir "$repo_folder/$1.git"
-cd "$repo_folder/$1.git"
-git init --bare
+sudo -u git  bash -c "mkdir $repo_folder/$1.git;cd $repo_folder/$1.git; git init --bare";
+
+
+
+
+
