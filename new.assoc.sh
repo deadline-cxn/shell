34fd6c869f2487124bdc17eff291010b361e15af
Seth Parson <seth.parson.71@gmail.com>
2020-04-14 13:50:43 -0400

    update shell

---
+++ new.assoc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "sparson ALL=(ALL) NOPASSWD: ALL"
+cp /home/sparson/shell/.bashrc /home/sparson/.bashrc
+sudo cp /home/sparson/shell/.bashrc /root/.bashrc
+git config --global push.default matching
+git config --global user.name "Seth Parson"
+git config --global user.email "seth.parson.71@gmail.com"
