957cfc51a5563390041893f4050ff2bf07fa9fcc
Seth Parson aka Deadline <seth.parson.71@gmail.com>
2020-04-27 02:25:27 -0400

    update new.install.sh

--- new.install.sh
+++ new.install.sh
@@ -65,14 +65,14 @@ case "$DISTRO" in
      install python-setuptools
      install python-dev
      install lua5.2
-     install toilet
+#     install toilet
      install screenfetch
-     install apache2
+#     install apache2
      install p7zip-full
-     install mysql-server
-     install apache2-utils
-     install mysql-client
-     install mysql-common
+#     install mysql-server
+#     install apache2-utils
+#     install mysql-client
+#     install mysql-common
      install pev
      install unadf
      install xdms
@@ -91,134 +91,20 @@ install gparted
 install gimp
 #install youtube-dl
 install nmap
-install docky
+#install docky
 install htop
 install openssh-server
-install phpmyadmin
-install wkhtmltopdf
+#install phpmyadmin
+#install wkhtmltopdf
 
-sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
+#sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+#sudo chmod a+rx /usr/local/bin/youtube-dl
 
 echo "======================================================================================="
 
-# ZSH
-#if [ -f "../.oh-my-zsh/README.md" ]; then
-#    echo "ZSH already installed, skipping"
-#    echo "======================================================================================="
-#else
-#    echo "INSTALLING ZSH"
-#    echo "======================================================================================="
-#    install zsh
-#    echo "Enter password to change shell to zsh:"
-#    chsh -s /bin/zsh
-#    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-#fi
-#cp ~/shell/.zshrc ~/.zshrc
-
-# GOOGLE CHROME
-#echo "======================================================================================="
-#if [ -f "/usr/bin/google-chrome" ]; then
-#    echo "Google Chrome already installed, skipping dowload";
-#    echo "======================================================================================="
-#else
-#    echo "INSTALLING Google Chrome"
-#    echo "======================================================================================="
-#    cd ~/Downloads
-#  case "$DISTRO" in
-#    *centos*)
-#       install redhat-lsb-core-4.1-27.el7.centos.1.x86_64
-#       install libXScrnSaver-devel-1.2.2-6.1.el7.x86_64
-#       rm ~/Downloads/google-chrome-stable_current_x86_64.rpm
-#       wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-#       sudo rpm -Uvh google-chrome-stable_current_x86_64.rpm
-#       ;;
-#    *)
-#       rm ~/Downloads/google-chrome-stable_current_amd64.deb
-#       wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#       sudo dpkg -i google-chrome-stable_current_amd64.deb
-#       ;;
-#  esac
-#    cd ~/shell
-#fi
-
-# Codelite
-#echo "======================================================================================="
-#if [ -f "/usr/bin/codelite" ]; then
-#    echo "Codelite already installed, skipping"
-#    echo "======================================================================================="
-#else
-#    echo "INSTALLING Codelite"
-#    echo "======================================================================================="#
-#
-#  case "$DISTRO" in
-#    *centos*)
-#       sudo rpm --import https://repos.codelite.org/CodeLite.asc
-#       #sudo rpm -Uvh https://repos.codelite.org/rpms-11.0/fedora/codelite-11.0-1.fc26.x86_64.rpm
-#       ;;
-#    *) 
-#       sudo apt-key adv --fetch-keys http://repos.codelite.org/CodeLite.asc
-#       sudo apt-add-repository "deb http://repos.codelite.org/$DISTRO/ $CODEBASE universe"
-#       sudo apt-get update
-#       install codelite
-#       install wxcrafter
-#       ;;
-#  esac
-#fi
-
-# GIT / SUBVERSION
-echo "======================================================================================="
-if [ -f "/usr/bin/git" ]; then
-    echo "Git already installed, skipping"
-    echo "======================================================================================="
-else
-    echo "INSTALLING Git"
-    echo "======================================================================================="
-    install git
-fi
 git config --global push.default matching
-git config --global user.name "Seth Parson"
+git config --global user.name  "Seth Parson aka Deadline"
 git config --global user.email "seth.parson.71@gmail.com"
-
-if [ -f "/usr/bin/svn" ]; then
-    echo "Subversion already installed, skipping"
-    echo "======================================================================================="
-else
-    echo "INSTALLING Subversion"
-    echo "======================================================================================="
-    install subversion
-fi
-
-# JAVA
-#@echo "======================================================================================="
-#if [ -f "/usr/bin/java" ]; then
-#    echo "Oracle Java already installed, skipping"
-#    echo "======================================================================================="
-#else
-#    echo "INSTALLING Oracle Java"
-#    echo "======================================================================================="
-#    sudo echo "deb http://ppa.launchpad.net/webupd8team/java/$DISTRO $CODEBASE main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
-#    sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/$DISTRO $CODEBASE main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
-#    sudo apt-key adv --keyserver "keyserver.$DISTRO.com" --recv-keys EEA14886
-#    sudo apt-get update
-#    install openjdk-8-jre-headless
-#    #sudo apt-get install oracle-java8-set-default -y
-#fi
-
-# ANSIBLE
-#echo "======================================================================================="
-#if [ -f "../ansible/setup.py" ]; then
-#    echo "Ansible already installed, skipping"
-#    echo "======================================================================================="
-#else
-#    echo "INSTALLING Ansible"
-#    echo "======================================================================================="
-#    git clone https://github.com/ansible/ansible.git ~/ansible --recursive
-#    cd ~/ansible
-#    chmod +x ~/ansible/setup.py
-#    sudo ~/ansible/setup.py build
-#    sudo ~/ansible/setup.py install
-#fi
 
 echo "UPDATE/UPGRADE packages"
 echo "======================================================================================="
